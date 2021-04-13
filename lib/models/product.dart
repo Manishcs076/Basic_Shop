26ea768651d882f45500970dd94836093a4e6d4b
manish123 <manishcs036@gmail.com>
2021-04-13 18:34:58 +0530

    Defining a Data Model for application

---
+++ lib/models/product.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Product {
+  final String id;
+  final String title;
+  final String description;
+  final String imageURL;
+  final double price;
+  bool isFavorite;
+
+  Product({
+    @required this.id,
+    @required this.title,
+    @required this.description,
+    @required this.imageURL,
+    @required this.price,
+    this.isFavorite = false,
+  });
+}
