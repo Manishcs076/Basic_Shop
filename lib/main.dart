7eb868ae78f87557ddc1ff1f158f945e8fcfbbfe
manish123 <manishcs036@gmail.com>
2021-04-14 18:56:36 +0530

    Styling and Theming the App

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,8 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.purple,
-        accentColor:  Colors.deepOrangeAccent,
+        accentColor: Colors.deepOrangeAccent,
+        fontFamily: 'Lato',
       ),
       home: ProductOverviewScreen(),
     );
