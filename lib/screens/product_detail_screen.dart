e0a61a87363f3a4d124a2420bb0db73b67ce264b
manish123 <manishcs036@gmail.com>
2021-04-20 23:05:52 +0530

    Adding Product Details for each products while  clicked on the products from main screen.

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -20,6 +20,42 @@ class ProductDetailScreen extends StatelessWidget {
           loadedProduct.title,
         ),
       ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: 300,
+              child: Image.network(
+                loadedProduct.imageUrl,
+                fit: BoxFit.cover,
+                width: double.infinity,
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              '₹${loadedProduct.price}',
+              style: TextStyle(
+                fontSize: 20,
+                color: Colors.grey,
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              width: double.infinity,
+              child: SizedBox(
+                height: 20,
+                child: Text(
+                  loadedProduct.description,
+                  textAlign: TextAlign.center,
+                  softWrap: true,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
