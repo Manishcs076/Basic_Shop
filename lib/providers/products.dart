6fffc56bb3e60a8fd0808a69a9de3dbd67a6a14e
manish123 <manishcs036@gmail.com>
2021-06-01 04:46:51 +0530

    working with Future(Dart)/ "async"

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -66,27 +66,33 @@ class Products with ChangeNotifier {
   // }
 
   void addProduct(Product product) {
-    const url = 'https://flutter-update-ce149-default-rtdb.firebaseio.com/products.json';
-    http.post(url,body: json.encode({
-      'title': product.title,
-      'description':product.description,
-      'imageUrl': product.imageUrl,
-      'price':product.price,
-      'isFavorite':product.isFavorite,
-    }),);
+    const url =
+        'https://flutter-update-ce149-default-rtdb.firebaseio.com/products.json';
+    http
+        .post(
+      url,
+      body: json.encode({
+        'title': product.title,
+        'description': product.description,
+        'imageUrl': product.imageUrl,
+        'price': product.price,
+        'isFavorite': product.isFavorite,
+      }),
+    )
+        .then((response) {
+     // print(json.decode(response.body));
+      final newProduct = Product(
+        title: product.title,
+        price: product.price,
+        imageUrl: product.imageUrl,
+        description: product.description,
+        id: json.decode(response.body)['name'],
+      );
 
-
-    final newProduct = Product(
-      title: product.title,
-      price: product.price,
-      imageUrl: product.imageUrl,
-      description: product.description,
-      id: DateTime.now().toString(),
-    );
-    
-    _items.add(newProduct);
-    // _items.insert(0, newProduct); //add items add the begining of the list
-    notifyListeners();
+      _items.add(newProduct);
+      // _items.insert(0, newProduct); //add items add the begining of the list
+      notifyListeners();
+    });
   }
 
   void updateProduct(String id, Product newProduct) {
