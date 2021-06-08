e071d4ff4f85c934e7870c25ba194dc6af0467c1
manish123 <manishcs036@gmail.com>
2021-06-08 12:55:05 +0530

    Fetching Orders from firebase and fixing an Issue.

--- lib/providers/orders.dart
+++ lib/providers/orders.dart
@@ -27,6 +27,40 @@ class Orders with ChangeNotifier {
     return [..._orders];
   }
 
+  Future<void> fetchAndSetOrders() async {
+    final url =
+        'https://flutter-update-ce149-default-rtdb.firebaseio.com/orders.json';
+    final response = await http.get(url);
+    final List<OrderItem> loadedOrders = [];
+    final extractedData = json.decode(response.body) as Map<String, dynamic>;
+    if (extractedData == null) {
+      return;
+    }
+    extractedData.forEach((orderId, orderData) {
+      loadedOrders.add(
+        OrderItem(
+          id: orderId,
+          amount: orderData['amount'],
+          products: (orderData['products'] as List<dynamic>)
+              .map(
+                (item) => CartItem(
+                  id: item['id'],
+                  title: item['title'],
+                  price: item['price'],
+                  quantity: item['quantity'],
+                ),
+              )
+              .toList(),
+          dateTime: DateTime.parse(
+            orderData['dateTime'],
+          ),
+        ),
+      );
+    });
+    _orders = loadedOrders.reversed.toList();
+    notifyListeners();
+  }
+
   Future<void> addOrder(List<CartItem> cartProducts, double total) async {
     final url =
         'https://flutter-update-ce149-default-rtdb.firebaseio.com/orders.json';
@@ -36,14 +70,14 @@ class Orders with ChangeNotifier {
       body: json.encode({
         'amount': total,
         'dateTime': timeStamp.toIso8601String(),
-        'products': cartProducts.map((cp)=>
-          {
-            'id': cp.id,
-            'title': cp.title,
-            'quantity': cp.quantity,
-            'price': cp.price,
-          }
-        ).toList(),
+        'products': cartProducts
+            .map((cp) => {
+                  'id': cp.id,
+                  'title': cp.title,
+                  'quantity': cp.quantity,
+                  'price': cp.price,
+                })
+            .toList(),
       }),
     );
     _orders.insert(
--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -71,6 +71,9 @@ class Products with ChangeNotifier {
     try {
       final response = await http.get(url);
       final extractedData = json.decode(response.body) as Map<String, dynamic>;
+      if (extractedData == null) {
+        return;
+      }
       final List<Product> loadedProducts = [];
       extractedData.forEach((prodId, prodData) {
         loadedProducts.add(Product(
@@ -150,7 +153,7 @@ class Products with ChangeNotifier {
         'https://flutter-update-ce149-default-rtdb.firebaseio.com/products/$id.json';
     final existingProductIndex = _items.indexWhere((prod) => prod.id == id);
     var existingProduct = _items[existingProductIndex];
-    
+
     _items.removeAt(existingProductIndex);
     notifyListeners();
 
