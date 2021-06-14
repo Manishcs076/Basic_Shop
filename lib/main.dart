f90b63a54f38fcfb6757c39d73f43962fe7ea913
manish123 <manishcs036@gmail.com>
2021-06-14 20:22:33 +0530

    Managing the auth Token Locally(in the App)

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import './screens/product_overview_screen.dart';
+
 import './providers/auth.dart';
 
 import './screens/auth_screen.dart';
@@ -39,23 +41,24 @@ class MyApp extends StatelessWidget {
           create: (ctx) => Orders(),
         ),
       ],
-      child: MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.purple,
-          accentColor: Colors.deepOrangeAccent,
-          fontFamily: 'Lato',
-        ),
-        home: AuthScreen(),
-        routes: {
-          ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
-          CartScreen.routeName: (ctx) => CartScreen(),
-          OrdersScreen.routeName: (ctx) => OrdersScreen(),
-          UserProductsScreen.routeName: (ctx) => UserProductsScreen(),
-          EditProductScreen.routeName: (ctx) => EditProductScreen(),
-          // AuthScreen.routeName:(ctx)=> AuthScreen(),
-        },
-      ),
+      child: Consumer<Auth>(builder: (ctx,auth,_)=> MaterialApp(
+            title: 'Flutter Demo',
+            theme: ThemeData(
+              primarySwatch: Colors.purple,
+              accentColor: Colors.deepOrangeAccent,
+              fontFamily: 'Lato',
+            ),
+            home: auth.isAuth ? ProductOverviewScreen() : AuthScreen(),
+            routes: {
+              ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
+              CartScreen.routeName: (ctx) => CartScreen(),
+              OrdersScreen.routeName: (ctx) => OrdersScreen(),
+              UserProductsScreen.routeName: (ctx) => UserProductsScreen(),
+              EditProductScreen.routeName: (ctx) => EditProductScreen(),
+              // AuthScreen.routeName:(ctx)=> AuthScreen(),
+            },
+          ),
+        )
     );
   }
 }
