826e86931706b027582d4bc46c250dc705c700d2
manish123 <manishcs036@gmail.com>
2021-06-27 09:15:32 +0530

    working with the  " AnimatedContainer"

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -218,17 +218,20 @@ class _AuthCardState extends State<AuthCard>
         borderRadius: BorderRadius.circular(10.0),
       ),
       elevation: 8.0,
-      child: AnimatedBuilder(
-        animation: _heightAnimation,
-        builder: (ctx, ch) => Container(
-            //height: _authMode == AuthMode.Signup ? 320 : 260,
-            height: _heightAnimation.value.height,
+      child:// AnimatedBuilder(
+       // animation: _heightAnimation,
+       // builder: (ctx, ch) =>
+        AnimatedContainer(
+          curve: Curves.easeIn,
+          duration: Duration(milliseconds: 300),
+            height: _authMode == AuthMode.Signup ? 320 : 260,
+           // height: _heightAnimation.value.height,
             constraints: BoxConstraints(
-                minHeight: _heightAnimation.value
-                    .height), // _authMode == AuthMode.Signup ? 320 : 260),
+                minHeight: // _heightAnimation.value
+                   // .height),
+                    _authMode == AuthMode.Signup ? 320 : 260),
             width: deviceSize.width * 0.75,
             padding: EdgeInsets.all(16.0),
-            child: ch),
         child: Form(
           key: _formKey,
           child: SingleChildScrollView(
